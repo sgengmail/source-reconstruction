ae8b125e413ec58385cf4fa88f9560933cd637a1
Shuang Geng <95016827+gengshuangmail@users.noreply.github.com>
2024-03-28 16:14:59 +0800

    Add files via upload

---
+++ select_peak_negsocial_min.m
@@ -0,0 +1,257 @@
+    exp_dir = '/bcbl/home/home_n-z/sgeng/MEGtool/toolboxes/';
+
+    % tbpath = '~/Dropbox/toolboxes/'
+    toolboxes = {'cartographie_motrice' 'fieldtrip-20170911'};
+    for tool = toolboxes
+        addpath(genpath(fullfile(exp_dir,tool{1})))  
+    end
+
+
+
+    
+    
+  subjects = {
+        'S1'
+        'S2';
+        'S3';
+         'S4'
+        'S5';
+        'S6';
+           'S7';
+        'S8';
+        'S9';
+         'S11'
+        'S12';
+        'S13';
+        }; 
+ 
+  
+    
+    raw_exp = 'G:\Premeg\shuang\Freesurfer';
+    meg_dir = 'G:\Premeg\shuang\MEG';
+    cd(fullfile(raw_exp));
+    subjects_dir = fullfile(raw_exp);
+    raw_dir =  fullfile(raw_exp,'raw');
+    exp = 'social';
+
+    
+    
+%     exp_dir = '/bcbl/home/home_n-z/sgeng/projects/naming';
+%     cd(fullfile(exp_dir,'FreeSurfer'));
+%     subjects_dir = fullfile(exp_dir,'FreeSurfer');
+%     meg_dir = fullfile(exp_dir,'meg');
+%     exp = 'naming';
+
+group_fold = fullfile(subjects_dir,exp)
+
+
+Nave = length(subjects);
+spm_file = [];
+for k = 1:Nave
+    spm_file{k} = fullfile(group_fold,[subjects{k} '_HS_beta.nii']);
+end
+
+Nave = length(subjects);
+spm_file_perm_stat = [];
+for k = 1:Nave
+    spm_file_perm_stat{k} = fullfile(group_fold,[subjects{k} '_LS_beta.nii']);
+end
+
+V = spm_vol(spm_file{1});
+Z = zeros([prod(V.dim) Nave]);
+for k = 1:Nave
+    V = spm_vol(spm_file{k});
+    C = log(spm_read_vols(V)+1);
+    Vp = spm_vol(spm_file_perm_stat{k});
+    Cp = log(spm_read_vols(Vp)+1);
+    Z(:,k) = (C(:)-Cp(:))/2;
+end
+
+
+NaNs = sum(isnan(Z),2);
+one_NaN = find(NaNs == 1);
+subZ = Z(one_NaN,:);
+subZ(isnan(subZ)) = 0;
+Z(one_NaN,:) = subZ;
+
+% sub_ind to speed up the processing
+sub_ind = ones(V.dim);
+set_to_0 = 1:V.dim(1);
+set_to_0(3:5:end) = [];
+sub_ind(set_to_0,:,:) = 0;
+sub_ind(:,set_to_0,:) = 0;
+sub_ind(:,:,set_to_0) = 0;
+sub_ind = find(sub_ind);
+to_rm = find(max(abs(Z(sub_ind,:)),[],2) == 0);
+sub_ind(to_rm) = [];
+
+% choose the permutation and start the loop for the permutation test
+Nsim = 1000;
+rand('seed',sum(clock)*1000)
+stat = zeros(1,Nsim+1);
+for k = 1:Nsim+1
+    if ~mod(k,20)
+        disp(['Processing simulation ' num2str(k) '/' num2str(Nsim)])
+        pause(0.0001)
+    end
+   
+    % determine permutations
+    perm = sign(randn(Nave,1));
+    if k == (Nsim + 1)
+        perm = ones(Nave,1);
+    end
+   
+    % Extract the statistic
+    sumZ = Z(sub_ind,:)*perm;
+    stat(k) = min(sumZ/Nave);    %%%%enlever abs si mesure l'un moins l'autre%%%
+end
+
+Zave = Z*perm/Nave;
+p = mean(min(Zave) > stat(1:Nsim));
+Th = prctile(stat(1:Nsim),01);
+Zave = reshape(Zave,V.dim);
+
+
+% write zmap. this part needs to be re-written
+W = V;
+W.dt(1) = 4;
+W.pinfo(1) = (max(Zave(:))-min(Zave(:)))/(2^15-1);
+W.pinfo(2) = min(Zave(:));
+W.pinfo(3) = 0;
+if ~exist(fullfile(group_fold,'group_ave'))
+    unix(['mkdir ' fullfile(group_fold,'group_ave')])
+end
+W.fname = fullfile(group_fold,'group_ave',['min' '_Th_' num2str(Th) '.nii']);
+W.private.dat.fname = W.fname;
+W = spm_write_vol(W,Zave);
+% 
+%% transform form Z to T
+
+
+
+%write a p-valed map
+sort_stat = sort(stat(1:Nsim),'descend');
+pmap_one = ones(size(Zave));
+keep = find(Zave > Th);
+pmap_one(keep) = 0;
+for ind = 1:Nsim*0.05;
+    pmap_one(keep) = pmap_one(keep) + (abs(Zave(keep)) >= sort_stat(ind))/Nsim;
+end
+pmap = pmap_one;
+pmap(pmap == 0) = 0.001;
+pmap = -log10(pmap);
+
+
+Va = W;
+Va.fname = fullfile(group_fold,'group_ave',['ventral''ventral_pmap.nii']);
+Va.private.dat.fname = Va.fname;
+Va.pinfo(1) = 3/(2^15-1);
+Va.pinfo(2) = 0;
+Va.pinfo(3) = 0;
+Va = spm_write_vol(Va,pmap);
+
+
+% % write a map thresholded a statistical significance
+% Zave = Zave.*(abs(Zave) > 0.19);
+% W.fname = fullfile(group_fold,'group_ave',['patents_' 'ventral_thresholded.nii']);
+% W = spm_write_vol(W,Zave);
+
+
+
+% smooth the map
+% smooth the map
+FWHM = 8;
+kern = CM_gaussian_kern(FWHM,W.dim);
+kern = kern/sum(kern(:));
+Fkern = fftn(kern);
+FZave = fftn(Zave);
+FZave = FZave.*Fkern;
+Zave = real(ifftn(FZave));
+
+[cluster_neg,d] = CM_local_maxima(W,abs(Zave),0.03);  % threshold of 0.13 fixed based on inspection of the map (to uncover the left SM1 source)
+
+%% manual selection
+keep = [1:10]; % manual selection (3 first are significant, last is added)
+cluster_neg = cluster_neg(keep);
+matfile = fullfile(group_fold,'group_ave',['cluster_group_pred']);
+save(matfile,'cluster_neg')
+
+clear rr_group_MNI
+
+% store the coordinates
+
+cluster=cluster_neg;
+
+coord_group = [];
+rr_coord_group = [];
+rr_group = [];
+for n_source = 1:length(cluster);
+    coord_group(:,n_source) = mean(cluster(n_source).mni,1);
+    rr_group_MNI(:,n_source) = round(mean(cluster(n_source).mni,1));
+    rr_group(:,n_source) = round(mean(cluster(n_source).vox,1));
+end
+
+mat_free2spm = eye(4); mat_free2spm(1:3,4) = [0 -16 20]; % see CM_overlay for the computation
+
+vals = zeros(length(cluster), length(subjects), 2);
+pvals = zeros(length(cluster),1);
+stats = cell(length(cluster),1);
+for n_source = 1:length(cluster);
+    for n_sub = 1:length(subjects)
+        to = subjects{n_sub};
+        from = 'MNI';
+        file1 = fullfile(group_fold,[subjects{n_sub} '_HS_beta.nii']);
+        file2 = fullfile(group_fold,[subjects{n_sub} '_LS_beta.nii']);
+       
+        V1 = spm_vol(file1);
+        V2 = spm_vol(file2);
+        C1 = spm_read_vols(V1);
+        C2 = spm_read_vols(V2);
+        Z = (C1(:)+C2(:))/2;
+        Nii_mat = reshape(Z,V1.dim);
+        clear Z;
+       
+        % smooth the maps
+        FWHM = 5;
+        kern = CM_gaussian_kern(FWHM,V1.dim);
+        kern = kern/sum(kern(:));
+        Fkern = fftn(kern);
+       
+        FNii_mat = fftn(Nii_mat);
+        FNii_mat = FNii_mat.*Fkern;
+        Nii_mat = real(ifftn(FNii_mat));
+        Z = Nii_mat;
+       
+        C1_mat = fftn(C1);
+        C1_mat = C1_mat.*Fkern;
+        D1 = real(ifftn(C1_mat));
+       
+        C2_mat = fftn(C2);
+        C2_mat = C2_mat.*Fkern;
+        D2 = real(ifftn(C2_mat));
+       
+        coord = [];
+        %for n_source = 1:length(cluster);
+        tmp = rr_group(:,n_source);   %% coordinates of 10 sourcess 
+        r = 7;
+        d = 2*r+1;
+        x = tmp(1)-r:tmp(1)+r; x = repmat(x',[1 d d]);                 x = x(:);
+        y = tmp(2)-r:tmp(2)+r; y = repmat(y,[d 1 d]);                  y = y(:);
+        z = tmp(3)-r:tmp(3)+r; z = repmat(reshape(z,[1 1 d]),[d d 1]); z = z(:);  %% XYZ
+        
+        rr = [x y z];
+        keep = find(sum((rr - repmat(tmp',[d^3 1])).^2,2)<r^2);
+        rr = rr(keep,:);
+        n = CM_xyz2n(rr,size(Z));  %Convert (x y z) coordinates of a 3D matrix of size sizemat into the    index of this matrix corresponding to (x y z)
+        %% n shi di n ge voxel
+        
+       [M,P] = max(Z(n));   % P index of maximum intensity, M the value of intensity  
+        E1 = D1(n);
+        E2 = D2(n);
+        %coord(:,n_source) = inv(mat_free2spm)*V1.mat*[rr(P,:) 1]';
+        vals(n_source, n_sub, :) = [E1(P) E2(P)];
+    end
+    %log_vals = log(vals);
+   
+    [h,pvals(n_source),ci,stats{n_source}] = ttest(vals(n_source,:,1), vals(n_source,:,2));
+end
---
+++ source_constructio_social.m
@@ -0,0 +1,374 @@
+    %%% from pipline source construction in MEGtools, edited by shuang
+    %%% 11-13-2018
+
+    exp_dir = '/bcbl/home/home_n-z/sgeng/toolbox/toolboxes/';
+
+    % tbpath = '~/Dropbox/toolboxes/'
+    toolboxes = {'cartographie_motrice' 'fieldtrip-20170911' 'freesurfer'};
+    for tool = toolboxes
+        addpath(genpath(fullfile(exp_dir,tool{1})))  
+    end
+
+
+
+
+    subject = {  
+'S1';
+         'S2';
+         'S3'        
+         'S4';
+         'S5';
+ 'S6';
+       'S7';
+       'S8'        
+         'S9';
+         'S11';
+ 'S12';
+       'S13';
+        };
+    
+   
+    
+
+    raw_exp = 'G:\Premeg\shuang\Freesurfer';
+    meg = 'G:\Premeg\shuang\MEG';
+    cd(fullfile(raw_exp));
+    subjects_dir = fullfile(raw_exp);
+
+    raw_dir =  fullfile(raw_exp,'raw');
+    exp = 'trans';
+
+
+
+
+    %% ADD PATHS
+    % add your fieldtrip
+
+    ft_defaults 
+    % since demean must be yes. 
+
+
+    for n_sub = 1:length(subject)
+        cfg=[]
+        cfg.subjects_dir = subjects_dir;
+        cfg.subject = subject{n_sub};
+        cfg.exp_name = exp;
+        cfg.meg_file = fullfile(raw_dir,[subject{n_sub} '_raw_tsss.fif']);
+        cfg.dicom = '';  % what is this 
+        cfg.space = 5;   % is this the grid size?
+        CM_mne_fwd_sol(cfg);
+         CM_mne_fwd_sol_from_MNI(cfg);
+    end
+
+    
+%          Foi=[13 28];
+%          Toi=[0.2 0.5];
+%          Tbase=[-0.3 -0.01];
+%     
+%     
+%     
+%     
+%     
+    
+ 
+%% head info
+%     load('/bcbl/home/home_n-z/sgeng/projects/naming/Naming_controls/filedtrip_data/sub1/meg/01_Euskera_Objects_tsss_mc.mat')
+%     grads = find(strcmp(data.grad.chantype,'megplanar'));
+%     magns = find(strcmp(data.grad.chantype,'megmag'));
+
+
+    
+    
+ clear Foi Toi Tbase
+ 
+ 
+         Foi=[20 21];
+         Toi=[0.2 0.35];
+         Tbase=[-0.3 -0.15];
+    
+    
+    
+    
+    
+ 
+%% head info
+%     load('/bcbl/home/home_n-z/sgeng/projects/naming/Naming_controls/filedtrip_data/sub1/meg/01_Euskera_Objects_tsss_mc.mat')
+%     grads = find(strcmp(data.grad.chantype,'megplanar'));
+%     magns = find(strcmp(data.grad.chantype,'megmag'));
+
+ for n_sub = 12:length(subject)
+  %% COV_noise
+
+    infile1 = fullfile(raw_exp,'raw',[subject{n_sub} '_raw_tsss.fif']);
+ 
+    oridata_obj=
+    load('S13_social.mat');  % pre=obj
+
+    
+    cfg        = [];
+    cfg.channel      = 'MEG';
+    data1   = ft_selectdata(cfg, HS);
+    
+    cfg        = [];
+    cfg.channel      = 'MEG';
+    data2   = ft_selectdata(cfg, LS);
+    
+    
+      %% baseline for obj
+     
+     COV_noise = [];
+     COV_noise.COV = zeros(306);     
+     keep_noise = find(data1.time{1} > min(Tbase) & data1.time{1} < max(Tbase));  % baseline boundaries defined here
+     for n_trial = 1:length(data1.trial)
+            COV_noise.COV = COV_noise.COV + data1.trial{n_trial}(:,keep_noise)*data1.trial{n_trial}(:,keep_noise)';
+     end
+     COV_noise.COV = COV_noise.COV/length(keep_noise)/n_trial;
+    
+    %%  baseline for verb
+     COV_noise2 = [];
+     COV_noise2.COV = zeros(306);     
+     keep_noise2 = find(data2.time{1} > min(Tbase) & data2.time{1} < max(Tbase));  % baseline boundaries defined here
+     for n_trial = 1:length(data2.trial)
+            COV_noise2.COV = COV_noise2.COV + data2.trial{n_trial}(:,keep_noise2)*data2.trial{n_trial}(:,keep_noise)';
+     end
+     COV_noise2.COV = COV_noise2.COV/length(keep_noise2)/n_trial;
+     
+   
+     fwdfile = fullfile(subjects_dir,subject{n_sub},'meg',[subject{n_sub},'_from_MNI-Mirror-5-src-fwd.fif'])
+     fwd = CM_lf2comp(fwdfile,COV_noise)
+
+%% CSD at the time of interest
+    % CSD at the time of interest  
+  
+         cfg        = [];
+    cfg.channel      = 'MEG';
+    cfg.latency = Toi;
+    data_obj   = ft_selectdata(cfg, HS);
+    data_verb  = ft_selectdata(cfg, LS);
+    cfg        = [];
+    cfg.channel      = 'MEG';
+    cfg.latency = Tbase;
+    data_bsl_obj   = ft_selectdata(cfg, HS);
+    data_bsl_verb  = ft_selectdata(cfg, LS);
+   
+    cfg = [];
+    cfg.channel   = 'MEG';
+    cfg.method    = 'mtmconvol';
+    cfg.output    = 'powandcsd';
+    cfg.taper =  'hanning';
+    cfg.foi  = 20:1:21  ;
+    cfg.toi = 0.2:0.01:0.35;
+    cfg.t_ftimwin =ones(length(cfg.foi),1).*0.5;
+    freq_source_obj = ft_freqanalysis(cfg,  HS);
+    freq_source_obj = CM_reshape_ft_CSD(freq_source_obj);
+    X = find(isnan(freq_source_obj.CSD));
+    cfg = [];
+    cfg.method    = 'mtmconvol';
+    cfg.channel   = 'MEG';
+    cfg.output    = 'powandcsd';
+    cfg.taper =  'hanning';
+    cfg.foi  = 20:1:21  ;
+    cfg.toi = -0.35:0.01:-0.2;
+    cfg.t_ftimwin = ones(length(cfg.foi),1).*0.5;
+    freq_source_base_obj = ft_freqanalysis(cfg, HS);
+    freq_source_base_obj = CM_reshape_ft_CSD(freq_source_base_obj);
+    X = find(isnan(freq_source_base_obj.CSD));
+    
+    
+   %% data_verb
+    cfg = [];
+    cfg.channel   = 'MEG';
+    cfg.method    = 'mtmconvol';
+    cfg.output    = 'powandcsd';
+    cfg.taper =  'hanning';
+    cfg.foi  = 20:1:21  ;
+    cfg.toi = 0.2:0.01:0.35;
+    cfg.t_ftimwin = ones(length(cfg.foi),1).*0.5;
+    freq_source_verb = ft_freqanalysis(cfg, LS);
+    freq_source_verb = CM_reshape_ft_CSD(freq_source_verb);
+    X = find(isnan(freq_source_verb.CSD));
+    
+    cfg = [];
+    cfg.channel   = 'MEG';
+    cfg.method    = 'mtmconvol';
+    cfg.output    = 'powandcsd';
+    cfg.taper =  'hanning';
+    cfg.foi  = 20:1:21  ;
+    cfg.toi = -0.35:0.01:-0.2;
+    cfg.t_ftimwin = ones(length(cfg.foi),1).*0.5;
+    freq_source_base_verb = ft_freqanalysis(cfg, LS);
+    freq_source_base_verb = CM_reshape_ft_CSD(freq_source_base_verb);
+    X = find(isnan(freq_source_base_verb.CSD));
+  
+    
+    CSD_pooled = [];
+    CSD_pooled.infile = infile1;
+    CSD_pooled.bads = [];
+    CSD_pooled.CSD = real(mean(freq_source_obj.CSD(:,:,:),3) + mean(freq_source_base_obj.CSD(:,:,:),3))/2;
+    Tinv.fwdfile = fwdfile;
+    Tinv = CM_sf_LCMV(Tinv,CSD_pooled);
+   
+    CSD_pooled = [];
+    CSD_pooled.infile = infile1;
+    CSD_pooled.bads = [];
+    CSD_pooled.CSD = real(mean(freq_source_verb.CSD(:,:,:),3) + mean(freq_source_base_verb.CSD(:,:,:),3))/2;
+    Tinv2.fwdfile = fwdfile;
+    Tinv2 = CM_sf_LCMV(Tinv2,CSD_pooled);
+    
+    
+    
+%% source power pre
+% Estimate the power in the source space (left and right multiplication
+% of the CSD by the inverse operator) first for the time of interest
+% and then for the baseline.
+      Nvox = double(Tinv.nsource);
+    % for the time of interest
+      P = zeros(1,Nvox);
+      T_CSD = Tinv.Tinv*mean(freq_source_obj.CSD(:,:,:),3);
+      for n = 1:Nvox
+          Cxx = T_CSD([-1 0]+2*n,:)*Tinv.Tinv([-1 0]+2*n,:)';
+          P(n) = max(abs(eig(Cxx)));
+      end
+    % for the baseline
+      Pbase = zeros(1,Nvox);
+      T_CSD = Tinv.Tinv*mean(freq_source_base_obj.CSD(:,:,:),3);
+        for n = 1:Nvox
+            Cxx = T_CSD([-1 0]+2*n,:)*Tinv.Tinv([-1 0]+2*n,:)';
+            Pbase(n) = max(abs(eig(Cxx)));
+        end
+        
+    obj = P
+    obj_base = Pbase
+%%
+
+%% source power post
+% Estimate the power in the source space (left and right multiplication
+% of the CSD by the inverse operator) first for the time of interest
+% and then for the baseline.
+      Nvox = double(Tinv2.nsource);
+    % for the time of interest
+      P = zeros(1,Nvox);
+      T_CSD = Tinv2.Tinv*mean(freq_source_verb.CSD(:,:,:),3);
+      for n = 1:Nvox
+          Cxx = T_CSD([-1 0]+2*n,:)*Tinv2.Tinv([-1 0]+2*n,:)';
+          P(n) = max(abs(eig(Cxx)));
+      end
+    % for the baseline
+      Pbase = zeros(1,Nvox);
+      T_CSD = Tinv2.Tinv*mean(freq_source_base_verb.CSD(:,:,:),3);
+        for n = 1:Nvox
+            Cxx = T_CSD([-1 0]+2*n,:)*Tinv2.Tinv([-1 0]+2*n,:)';
+            Pbase(n) = max(abs(eig(Cxx)));
+        end
+    
+      verb = P
+      verb_base = Pbase
+%% Estimate the power ratio and take care of that some sources are ouside 
+ %the bem
+   
+      fwd = mne_read_forward_solution(fwdfile)
+%     
+      Pratio_verb =zeros(1,fwd.src.np);   % change the fwd
+      Pratio_verb(fwd.src.vertno) = (verb - verb_base)./verb_base   ;
+      
+      Pratio_obj =zeros(1,fwd.src.np);   % change the fwd
+      Pratio_obj(fwd.src.vertno) = (obj - obj_base)./obj_base ;
+     
+%       Pratio_verb_obj =zeros(1,fwd.src.np);   % change the fwd
+%       Pratio_verb_obj(fwd.src.vertno) = (verb - obj)./(obj_base + verb_base) ;
+%     
+%     
+%       Pratio_obj_verb =zeros(1,fwd.src.np);   % change the fwd
+%       Pratio_obj_verb(fwd.src.vertno) = (obj - verb)./(obj_base + verb_base) ;
+    
+%%
+    % convert the vector Pratio into a 3D overlay corregistered to MNI MRI
+      map_mri_verb = CM_overlay_MNI((Pratio_verb)',5,subjects_dir);   
+      map_mri_verb(map_mri_verb==0) = 0; % set the power ratio to 1 for voxels outside the BEM
+%     
+      map_mri_obj = CM_overlay_MNI((Pratio_obj)',5,subjects_dir);   
+      map_mri_obj(map_mri_obj==0) = 0; % set the power ratio to 1 for voxels outs
+%     
+%       map_mri_verb_obj = CM_overlay_MNI((Pratio_verb_obj)',5,subjects_dir);   
+%       map_mri_verb_obj(map_mri_verb_obj==0) = 0; % set the power ratio to 1 for voxels outs
+%     
+%     
+%       map_mri_obj_verb = CM_overlay_MNI((Pratio_obj_verb)',5,subjects_dir);   
+%       map_mri_obj_verb(map_mri_obj_verb==0) = 0; % set the power ratio to 1 for voxels outs
+% %     
+    %% write down the overlay. Overlays starticlearng with 'w' are normalized to
+    % MNI template. Those starting with 'iw' are normalized to subject's
+    % anatomy and should be visualized on the 'orig' MRI available in the
+    S = spm_vol(fullfile(subjects_dir , 'MNI' , 'norm' , 'MNI.nii'));
+    if not(exist(fullfile(subjects_dir , subject{n_sub} , exp)))
+        mkdir(fullfile(subjects_dir , subject{n_sub} , exp));
+    end
+    
+%     map_mri = map_mri_obj
+%     
+%     S.fname = fullfile(subjects_dir, exp,[subjects{n_sub} 'control_obj_theta.nii']);
+%     S.dt(1) = 4;
+%     S.pinfo(1) = max(map_mri(:))/(2^15-1);
+%     S.pinfo(2) = 0;
+%     S.pinfo(3) = 0;
+%     S.private.dat.fname = S.fname;
+%     S = spm_write_vol(S,map_mri);
+% %     load(fullfile(subjects_dir , subjsubjectsorderects{n_sub} , 'norm' , 'inverse_norm.mat'))
+% %     dnrm.write.prefix = 'i';
+% %     spm_write_sn(S.fname,iprm,dnrm.write);
+% %     
+%     clear map_mri
+   outputfile = 'social'; 
+    map_mri = map_mri_verb;
+    
+    S.fname = fullfile(subjects_dir, outputfile,[subject{n_sub} '_LS_beta.nii']);
+    S.dt(1) = 4;
+    S.pinfo(1) = max(map_mri(:))/(2^15-1);
+    S.pinfo(2) = 0;
+    S.pinfo(3) = 0;
+    S.private.dat.fname = S.fname;
+    S = spm_write_vol(S,map_mri);
+%     load(fullfile(subjects_dir , subjects{n_sub} , 'norm' , 'inverse_norm.mat'))
+%     dnrm.write.prefix = 'i';
+%     spm_write_sn(S.fname,iprm,dnrm.write);
+    
+    clear map_mri
+    
+%      
+    map_mri = map_mri_obj;
+%     
+    S.fname = fullfile(subjects_dir, outputfile,[subject{n_sub} '_HS_beta.nii']);
+    S.dt(1) = 4;
+    S.pinfo(1) = max(map_mri(:))/(2^15-1);
+    S.pinfo(2) = 0;
+    S.pinfo(3) = 0;
+    S.private.dat.fname = S.fname;
+    S = spm_write_vol(S,map_mri);
+%     load(fullfile(subjects_dir , subjects{n_sub} , 'norm' , 'inverse_norm.mat'))
+%     dnrm.write.prefix = 'i';
+%     spm_write_sn(S.fname,iprm,dnrm.write);
+% 
+% %         
+%     clear map_mri
+%     
+%     map_mri = map_mri_obj_verb
+%     
+%     S.fname = fullfile(subjects_dir, exp,[subjects{n_sub} 'control_obj_verb.nii']);
+%     S.dt(1) = 4;
+%     S.pinfo(1) = max(map_mri(:))/(2^15-1);
+%     S.pinfo(2) = 0;
+%     S.pinfo(3) = 0;
+%     S.private.dat.fname = S.fname;
+%     S = spm_write_vol(S,map_mri);
+%     load(fullfile(subjects_dir , subjects{n_sub} , 'norm' , 'inverse_norm.mat'))
+%     dnrm.write.prefix = 'i';
+%     spm_write_sn(S.fname,iprm,dnrm.write);
+
+%         
+    clear map_mri
+    
+    
+    
+ end
+ 
+ 
