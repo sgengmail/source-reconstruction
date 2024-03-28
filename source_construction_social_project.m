477490cccc845be2104df10cb4171b5642cdfa8e
Shuang Geng <95016827+gengshuangmail@users.noreply.github.com>
2024-03-28 16:22:23 +0800

    Update and rename source_constructio_social.m to source_construction_social_project.m

--- source_construction_social_project.m
+++ source_construction_social_project.m
@@ -1,35 +1,32 @@
-    %%% from pipline source construction in MEGtools, edited by shuang
-    %%% 11-13-2018
+%%% from pipline source construction in MEGtools
+%%% 
+exp_dir = '/bcbl/home/home_n-z/sgeng/toolbox/toolboxes/';
 
-    exp_dir = '/bcbl/home/home_n-z/sgeng/toolbox/toolboxes/';
-
-    % tbpath = '~/Dropbox/toolboxes/'
-    toolboxes = {'cartographie_motrice' 'fieldtrip-20170911' 'freesurfer'};
-    for tool = toolboxes
-        addpath(genpath(fullfile(exp_dir,tool{1})))  
-    end
-
+% tbpath = '~/Dropbox/toolboxes/'
+toolboxes = {'cartographie_motrice' 'fieldtrip-20170911' 'freesurfer'};
+for tool = toolboxes   
+   addpath(genpath(fullfile(exp_dir,tool{1})))  
+end
 
 
-
-    subject = {  
-'S1';
+subject = {  
+         'S1';
          'S2';
          'S3'        
          'S4';
          'S5';
- 'S6';
-       'S7';
-       'S8'        
-         'S9';
-         'S11';
- 'S12';
+         'S6';
+        'S7';
+        'S8'        
+        'S9';
+          'S11';
+          'S12';
        'S13';
         };
     
    
     
-
+ 
     raw_exp = 'G:\Premeg\shuang\Freesurfer';
     meg = 'G:\Premeg\shuang\MEG';
     cd(fullfile(raw_exp));
@@ -47,7 +44,7 @@
     ft_defaults 
     % since demean must be yes. 
 
-
+%% check automaticly which step is not be done.
     for n_sub = 1:length(subject)
         cfg=[]
         cfg.subjects_dir = subjects_dir;
@@ -61,17 +58,9 @@
     end
 
     
-%          Foi=[13 28];
-%          Toi=[0.2 0.5];
-%          Tbase=[-0.3 -0.01];
-%     
-%     
-%     
-%     
-%     
-    
- 
-%% head info
+
+
+%% Load head info if it is neccessary, normally dont need 
 %     load('/bcbl/home/home_n-z/sgeng/projects/naming/Naming_controls/filedtrip_data/sub1/meg/01_Euskera_Objects_tsss_mc.mat')
 %     grads = find(strcmp(data.grad.chantype,'megplanar'));
 %     magns = find(strcmp(data.grad.chantype,'megmag'));
@@ -79,25 +68,19 @@
 
     
     
- clear Foi Toi Tbase
+clear Foi Toi Tbase
  
  
-         Foi=[20 21];
-         Toi=[0.2 0.35];
-         Tbase=[-0.3 -0.15];
+Foi=[20 28];
+Toi=[0.2 0.35];
+Tbase=[-0.3 -0.15];
     
     
     
     
+for n_sub = 12:length(subject)
     
- 
-%% head info
-%     load('/bcbl/home/home_n-z/sgeng/projects/naming/Naming_controls/filedtrip_data/sub1/meg/01_Euskera_Objects_tsss_mc.mat')
-%     grads = find(strcmp(data.grad.chantype,'megplanar'));
-%     magns = find(strcmp(data.grad.chantype,'megmag'));
-
- for n_sub = 12:length(subject)
-  %% COV_noise
+    %% COV_noise
 
     infile1 = fullfile(raw_exp,'raw',[subject{n_sub} '_raw_tsss.fif']);
  
@@ -264,7 +247,7 @@
       verb = P
       verb_base = Pbase
 %% Estimate the power ratio and take care of that some sources are ouside 
- %the bem
+
    
       fwd = mne_read_forward_solution(fwdfile)
 %     
@@ -274,28 +257,14 @@
       Pratio_obj =zeros(1,fwd.src.np);   % change the fwd
       Pratio_obj(fwd.src.vertno) = (obj - obj_base)./obj_base ;
      
-%       Pratio_verb_obj =zeros(1,fwd.src.np);   % change the fwd
-%       Pratio_verb_obj(fwd.src.vertno) = (verb - obj)./(obj_base + verb_base) ;
-%     
-%     
-%       Pratio_obj_verb =zeros(1,fwd.src.np);   % change the fwd
-%       Pratio_obj_verb(fwd.src.vertno) = (obj - verb)./(obj_base + verb_base) ;
-    
-%%
+
     % convert the vector Pratio into a 3D overlay corregistered to MNI MRI
       map_mri_verb = CM_overlay_MNI((Pratio_verb)',5,subjects_dir);   
       map_mri_verb(map_mri_verb==0) = 0; % set the power ratio to 1 for voxels outside the BEM
 %     
       map_mri_obj = CM_overlay_MNI((Pratio_obj)',5,subjects_dir);   
       map_mri_obj(map_mri_obj==0) = 0; % set the power ratio to 1 for voxels outs
-%     
-%       map_mri_verb_obj = CM_overlay_MNI((Pratio_verb_obj)',5,subjects_dir);   
-%       map_mri_verb_obj(map_mri_verb_obj==0) = 0; % set the power ratio to 1 for voxels outs
-%     
-%     
-%       map_mri_obj_verb = CM_overlay_MNI((Pratio_obj_verb)',5,subjects_dir);   
-%       map_mri_obj_verb(map_mri_obj_verb==0) = 0; % set the power ratio to 1 for voxels outs
-% %     
+%       
     %% write down the overlay. Overlays starticlearng with 'w' are normalized to
     % MNI template. Those starting with 'iw' are normalized to subject's
     % anatomy and should be visualized on the 'orig' MRI available in the
@@ -304,21 +273,8 @@
         mkdir(fullfile(subjects_dir , subject{n_sub} , exp));
     end
     
-%     map_mri = map_mri_obj
-%     
-%     S.fname = fullfile(subjects_dir, exp,[subjects{n_sub} 'control_obj_theta.nii']);
-%     S.dt(1) = 4;
-%     S.pinfo(1) = max(map_mri(:))/(2^15-1);
-%     S.pinfo(2) = 0;
-%     S.pinfo(3) = 0;
-%     S.private.dat.fname = S.fname;
-%     S = spm_write_vol(S,map_mri);
-% %     load(fullfile(subjects_dir , subjsubjectsorderects{n_sub} , 'norm' , 'inverse_norm.mat'))
-% %     dnrm.write.prefix = 'i';
-% %     spm_write_sn(S.fname,iprm,dnrm.write);
-% %     
-%     clear map_mri
-   outputfile = 'social'; 
+
+    outputfile = 'social'; 
     map_mri = map_mri_verb;
     
     S.fname = fullfile(subjects_dir, outputfile,[subject{n_sub} '_LS_beta.nii']);
@@ -328,15 +284,13 @@
     S.pinfo(3) = 0;
     S.private.dat.fname = S.fname;
     S = spm_write_vol(S,map_mri);
-%     load(fullfile(subjects_dir , subjects{n_sub} , 'norm' , 'inverse_norm.mat'))
-%     dnrm.write.prefix = 'i';
-%     spm_write_sn(S.fname,iprm,dnrm.write);
+
     
     clear map_mri
     
-%      
+ 
     map_mri = map_mri_obj;
-%     
+    
     S.fname = fullfile(subjects_dir, outputfile,[subject{n_sub} '_HS_beta.nii']);
     S.dt(1) = 4;
     S.pinfo(1) = max(map_mri(:))/(2^15-1);
@@ -344,31 +298,9 @@
     S.pinfo(3) = 0;
     S.private.dat.fname = S.fname;
     S = spm_write_vol(S,map_mri);
-%     load(fullfile(subjects_dir , subjects{n_sub} , 'norm' , 'inverse_norm.mat'))
-%     dnrm.write.prefix = 'i';
-%     spm_write_sn(S.fname,iprm,dnrm.write);
-% 
-% %         
-%     clear map_mri
-%     
-%     map_mri = map_mri_obj_verb
-%     
-%     S.fname = fullfile(subjects_dir, exp,[subjects{n_sub} 'control_obj_verb.nii']);
-%     S.dt(1) = 4;
-%     S.pinfo(1) = max(map_mri(:))/(2^15-1);
-%     S.pinfo(2) = 0;
-%     S.pinfo(3) = 0;
-%     S.private.dat.fname = S.fname;
-%     S = spm_write_vol(S,map_mri);
-%     load(fullfile(subjects_dir , subjects{n_sub} , 'norm' , 'inverse_norm.mat'))
-%     dnrm.write.prefix = 'i';
-%     spm_write_sn(S.fname,iprm,dnrm.write);
-
-%         
+        
     clear map_mri
-    
-    
-    
+       
  end
  
  
